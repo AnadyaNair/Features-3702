33d2ab783fd046de54995342af825b369131e34c
AnadyaNair✔ <71205867+AnadyaNair@users.noreply.github.com>
2022-02-01 18:54:23 +0530

    Add: README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+<img src="Features Banner.svg">
+</p>
+
+## Regarding
+
+Features is an open source Discord bot. It was made for people getting started with Discord. The bot contains a directory of Discord's features, including secret tricks that are hidden and can be performed using the application. The bot is currently under development, and is not yet at the stage of deployment. However, people can still use it in it's BETA phase.
+
+## Coverage
+
+- NPM v16 and v16 + version.
+- Discord.js v13
+- Discord API
+
+## Add The Bot To A Discord Server
+
+1. Open this link address → https://discord.com/api/oauth2/authorize?client_id=937258591189106698&permissions=137439333440&scope=bot
+2. Select a server.
+3. Click on continue.
+4. Approve the permissions listed, it is recommended to aprove all the permissions in order for the bot to function properly.
+5. Click Authorise.
+
+## Supported Commands
+
+`+start`
--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token, Prefix } = require('./config.json');
+const { Client, Intents } = require("discord.js");
+const { token, Prefix } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, 'GUILDS', 'GUILD_MESSAGES'] });
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
-    client.user.setActivity('+start', { type: 'LISTENING' });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, "GUILDS", "GUILD_MESSAGES"],
 });
 
-client.on('messageCreate', (message) => {
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+  client.user.setActivity("+start", { type: "LISTENING" });
+});
 
-    if (message.content == Prefix + "ping") {
-        message.reply(`Pong 🏓! **${client.ws.ping}ms**.`);
-    }
+client.on("messageCreate", (message) => {
+  if (message.content == Prefix + "ping") {
+    message.reply(`Pong 🏓! **${client.ws.ping}ms**.`);
+  }
 
-    if (message.content == Prefix + "start") {
-        const { MessageEmbed } = require('discord.js');
+  if (message.content == Prefix + "start") {
+    const { MessageEmbed } = require("discord.js");
 
-        // inside a command, event listener, etc.
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#57F287')
-            .setTitle('Hey there, I am Features.')
-            .setAuthor({ name: 'Features', iconURL: 'https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true' })
-            .setDescription('Hey there, I am Features. I was built to help you get started with Discord. This is my directory from where you can pick your Discord lesson. Remember to use my prefix "+" while you call me.\n\nPlease Note: I am not a bot made by the official Discord staff.')
-            .setThumbnail('https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true')
-            .addFields(
-                { name: 'Features Lesson Directory', value: '``+lessons``', inline: true},
-                // { name: '\u200B', value: '\u200B' },
-                // { name: 'Inline field title', value: 'Some value here', inline: true },
-                // { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            // .addField('Inline field title', 'Some value here', true)
-            // .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            .setFooter({ text: 'Hey there, I am Features.', iconURL: 'https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true' });
+    // inside a command, event listener, etc.
+    const exampleEmbed = new MessageEmbed()
+      .setColor("#57F287")
+      .setTitle("Hey there, I am Features.")
+      .setAuthor({
+        name: "Features",
+        iconURL:
+          "https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true",
+      })
+      .setDescription(
+        'Hey there, I am Features. I was built to help you get started with Discord. This is my directory from where you can pick your Discord lesson. Remember to use my prefix "+" while you call me.\n\nPlease Note: I am not a bot made by the official Discord staff.'
+      )
+      .setThumbnail(
+        "https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true"
+      )
+      .addFields(
+        {
+          name: "Features Lesson Directory",
+          value: "``+lessons``",
+          inline: true,
+        }
+        // { name: '\u200B', value: '\u200B' },
+        // { name: 'Inline field title', value: 'Some value here', inline: true },
+        // { name: 'Inline field title', value: 'Some value here', inline: true },
+      )
+      // .addField('Inline field title', 'Some value here', true)
+      // .setImage('https://i.imgur.com/AfFp7pu.png')
+      .setTimestamp()
+      .setFooter({
+        text: "Hey there, I am Features.",
+        iconURL:
+          "https://github.com/AnadyaNair/Features-3702/blob/main/Features-Bot-Logo.png?raw=true",
+      });
 
-        message.reply({ embeds: [exampleEmbed] });
-        // message.reply('Hey there, I am Features. I was built to help you get started with Discord. This is my directory from where yo can pick your lesson. Remember to use my prefix "+" while you call me.');
-    }
-})
+    message.reply({ embeds: [exampleEmbed] });
+    // message.reply('Hey there, I am Features. I was built to help you get started with Discord. This is my directory from where yo can pick your lesson. Remember to use my prefix "+" while you call me.');
+  }
+});
 
 // Login to Discord with your client's token
 client.login(token);
